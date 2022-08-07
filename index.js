e1c5df713dcc1371992e8882f9bf31fa03956826
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-08-06 20:32:59 -0400

    Changed API and html

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const BASE_URL = 'https://developers.teleport.org/api/'
+const BASE_URL = 'http://api.tvmaze.com'
+
+// 'https://api.teleport.org/api/urban_areas/?embed=ua:item/ua:images'
 
 window.addEventListener('DOMContentLoaded', () => {
-  getCityData()
+  getShowList()
 })
 
-function getCityData() {
-  const ul = document.getElementById('city-list')
+function getShowList() {
+  const ul = document.getElementById('show-list')
 
-  fetch (BASE_URL + '/cities/')
+   fetch(BASE_URL + '/shows')
   .then(res => res.json())
-  data.forEach(city => {
-    ul.innerHTML +=
-      <li>${city.name}</li>
-  })
+  .then(data => {
+        console.log(data)
+        // data.forEach(show => {
+        // ul.innerHTML += `
+        // <li>${show.name}</li>
+        // `
+     })
   }
 
 
+// GET --header "Accept: application/vnd.teleport.v1+json" "https://api.teleport.org/api/cities/?search=Baltimore%2C%20Atlanta%2C%20Dallas%2C%20Charlotte%2C%20Washington%20DC%2C%20Philadelphia%2C%20Nashville&limit=7"
+
+// https://api.teleport.org/api/cities/?search=Baltimore%2C%20Atlanta%2C%20Dallas%2C%20Charlotte%2C%20Washington%20DC%2C%20Philadelphia%2C%20Nashville&limit=7
