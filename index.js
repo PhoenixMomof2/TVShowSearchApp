a5c4fcd50a1e9847319d8cac478325c77ac9e84c
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-08-08 12:37:41 -0400

    Successfully filtering the shows by genres, but unable to render them to page upon click event.

--- index.js
+++ index.js
@@ -1,26 +1,71 @@
 const BASE_URL = 'http://api.tvmaze.com'
+let showData = []
+let filteredData = []
 
 window.addEventListener('DOMContentLoaded', () => {
-  searchShowList()
+  displayAllShows()
+  // document.getElementsByClassName("btn").addEventListener('click', displayShowList)
 })
 
-function searchShowList(query) {
+function displayAllShows() {
   const ul = document.getElementById('show-list')
 
-   fetch(BASE_URL + `/search/shows?q=${query}`)
+   fetch(BASE_URL + `/shows`)
   .then(response => response.json())
   .then(data => {
         // console.log(data)
-        data.filter(show => {
+        showData = data
+        // console.log(showData)
+        data.forEach(show => {
+          // console.log(show.genres)
         ul.innerHTML += `
-        <li>${show.name}</li>
+        <li><a href="#">${show.name}</a></li>
         `
      })
+    //  clickShowLinks()
   })
 }
 
-// button.addEventListener('click', () => {
-//   function listSciFiShows (e) {
+function filterSelection(genre) {
+  // console.log(genre)
+  let filteredData = showData.filter((show) => {return show.genres.includes(genre)
+  })
+   console.log(filteredData)
+    // filteredData.filter(show => {
+    //     ul.innerHTML += `
+    //     <li><a href="#">${show.name}</a></li>
+    //     ` 
+    // })
+    // clickShowLinks()
+  }
+  
 
-//   }
-// })
+
+
+
+
+// const clickShowLinks = () => {
+//     const shows = document.querySelectorAll('a')
+//     shows.forEach((show) => {
+//         show.addEventListener('click', displaySelectedShow)
+//     })
+// }
+
+// const displaySelectedShow = (event) => {
+//     // console.log(event.target.dataset.genres)
+//     const details = document.getElementById('details')
+//     const ul = document.getElementById('show-list')
+//     ul.innerHTML = ''
+
+    // fetch(BASE_URL + `/shows/${event.target.dataset.genres}`)
+    // .then(response => response.json())
+    // .then(data => {
+    //    console.log(data)
+    // })
+// }
+
+
+
+// data-id adds the id index of the show so only one show is fetched with id is used.
+// Each show is added to the list as a link.
+// href # takes you to the top of the page.
